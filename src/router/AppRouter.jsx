a99d45d5ed48643728d4a3e1b7e6604cf2fcec65
HwangJiYoung <start014stop@naver.com>
2026-06-17 19:37:05 +0900

    feat: 라우팅 충돌 해결

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,19 +2,35 @@ import { Navigate, Route, Routes } from "react-router-dom";
 
 import HomePage from "../features/mainPage/pages/HomePage";
 import RestaurantDetailPage from "../features/mainPage/pages/RestaurantDetailPage";
+import LoginPage from "../features/loginPage/pages/LoginPage/LoginPage";
+
+import EmailCheckPage from "../features/loginPage/pages/signupPage/EmailCheckPage/EmailCheckPage";
+
+import PasswordSettingPage from "../features/loginPage/pages/signupPage/PasswordSettingPage/PasswordSettingPage";
+
+import NicknameSettingPage from "../features/loginPage/pages/signupPage/NicknameSettingPage/NicknameSettingPage";
+
+import SignupSuccessPage from "../features/loginPage/pages/signupPage/SignupSuccessPage/SignupSuccessPage";
+import MyPage from "../features/myPage/pages/MyPage";
 
 import { ROUTES } from "./routes.constant";
 
 function AppRouter() {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to={ROUTES.HOME} replace />} />
+      <Route path="/" element={<Navigate to={ROUTES.LOGIN} replace />} />{" "}
       <Route path={ROUTES.HOME} element={<HomePage />} />
-
+      <Route path={ROUTES.HOME} element={<HomePage />} />
+      <Route path={ROUTES.MYPAGE} element={<MyPage />} />
       <Route
         path={ROUTES.RESTAURANT_DETAIL()}
         element={<RestaurantDetailPage />}
       />
+      <Route path={ROUTES.LOGIN} element={<LoginPage />} />
+      <Route path={ROUTES.SIGNUP_EMAIL} element={<EmailCheckPage />} />
+      <Route path={ROUTES.SIGNUP_PASSWORD} element={<PasswordSettingPage />} />
+      <Route path={ROUTES.SIGNUP_NICKNAME} element={<NicknameSettingPage />} />
+      <Route path={ROUTES.SIGNUP_SUCCESS} element={<SignupSuccessPage />} />
     </Routes>
   );
 }
--- src/router/routes.constant.js
+++ src/router/routes.constant.js
@@ -3,6 +3,16 @@ export const ROUTES = Object.freeze({
 
   MYPAGE: "/mypage",
 
+  SIGNUP_EMAIL: "/signup/email",
+
+  SIGNUP_PASSWORD: "/signup/password",
+
+  SIGNUP_NICKNAME: "/signup/nickname",
+
+  SIGNUP_SUCCESS: "/signup/success",
+
+  LOGIN: "/login",
+
   RESTAURANT_DETAIL: (restaurantId = ":restaurantId") =>
     `/restaurants/${restaurantId}`,
 });
